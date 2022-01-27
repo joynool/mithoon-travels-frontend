d2de62e669b5066372946f326182c3db07c1fd20
JoynoolAbedinMithoon <mithun55@gmail.com>
2022-01-27 22:26:03 +0600

    carousel banner completed

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import Slider from 'react-slick';
+import one from './../../images/one.jpg';
+import two from './../../images/two.jpg';
+import three from './../../images/three.jpg';
+import four from './../../images/four.jpg';
 
 const Banner = () =>
 {
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 3000,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
-        <div>
-            <h2>hello from banner</h2>
+        <div className='m-10'>
+            <Slider {...settings}>
+                <div>
+                    <img src={one} alt="slider one" />
+                </div>
+                <div>
+                    <img src={two} alt="slider one" />
+                </div>
+                <div>
+                    <img src={three} alt="slider one" />
+                </div>
+                <div>
+                    <img src={four} alt="slider one" />
+                </div>
+            </Slider>
         </div>
     );
 };
